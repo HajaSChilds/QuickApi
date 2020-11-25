a7d713f544adfd6f281037516facaf318d6c225f
HajaSC <hajasc@protonmail.com>
2020-11-25 00:12:48 -0500

    Initial GET and POST methods, confirmed working  by Postman

--- QuickApi/api_basic/views.py
+++ QuickApi/api_basic/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from rest_framework.parsers import JSONParser
-from .models import Article
 from .serializers import ArticleSerializer
+from .models import Article
 
 # Create your views here.
 
@@ -16,9 +16,9 @@ def article_list(request):
 
     elif request.method =='POST':
         data = JSONParser().parse(request)
-        serializer = Article Serializer(data=data)  
+        serializer = ArticleSerializer(data=data)  
 
         if serializer.is_valid():
             serializer.save()
             return JsonResponse(serializer.data, status=201)
-            return JsonResponse(serializer)  
+    return JsonResponse(serializer.errors, status=400)  
